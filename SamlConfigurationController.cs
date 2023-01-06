29bb2d1537633efc0b492302ac8bc49054e3a59a
Benjamin Juul Jensen <43336954+benjaminjuul@users.noreply.github.com>
2023-01-06 15:50:51 +0100

    Add files via upload

---
+++ SamlConfigurationController.cs
@@ -0,0 +1,320 @@
+﻿using ComponentSpace.Saml2.Configuration;
+using ComponentSpace.Saml2.Configuration.Database;
+using ComponentSpace.Saml2.Utility;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using EMSuite.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace EMSuite.Controllers
+{
+    public class SamlConfigurationController : Controller
+    {
+        private readonly SamlConfigurationContext _context;
+
+        public SamlConfigurationViewModel model = new SamlConfigurationViewModel();
+
+
+        public SamlConfigurationController(SamlConfigurationContext _context)
+        {
+            this._context = _context;
+        }
+
+       
+
+        //public void OnGet()
+        //{
+        //    DatabaseSeeded = samlConfigurationContext.SamlConfigurations.Count() > 0;
+
+        //    string licenseType;
+
+        //    if (license.IsLicensed)
+        //    {
+        //        licenseType = "Licensed";
+        //    }
+        //    else
+        //    {
+        //        licenseType = $"Evaluation (Expires {license.Expires.ToShortDateString()})";
+        //    }
+
+        //    ProductInformation = $"ComponentSpace.Saml2, Version={license.Version}, {licenseType}";
+        //}
+
+        // GET: SamlConfigurations
+        [HttpGet]
+        public async Task<IActionResult> Index(SamlConfigurationViewModel model)
+        {
+
+            var data = _context.SamlConfigurations
+                .Include(p => p.PartnerIdentityProviderConfigurations)
+                .First().PartnerIdentityProviderConfigurations.ToList();
+
+            return View(data);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(SamlConfigurationViewModel model)
+        {
+
+
+            //if (_context.SamlConfigurations.Count() != 0)
+            //{
+            //try
+            //{
+
+            try
+            {
+                
+
+                var data = _context.SamlConfigurations
+                .Include(i => i.LocalServiceProviderConfiguration).Where(l => l.Id == 2027).First();
+
+
+                if (data != null)
+                {
+
+
+                    var partnerIdentityProvider = new PartnerIdentityProviderConfiguration()
+                    {
+                        Name = model.Name,
+                        Description = model.Description,
+                        SignAuthnRequest = true,
+                        SignLogoutRequest = true,
+                        SignLogoutResponse = true,
+                        SingleSignOnServiceUrl = model.SingleSignOnServiceUrl,
+                        SingleLogoutServiceUrl = model.SingleLogoutServiceUrl,
+                        PartnerCertificates = new List<Certificate>()
+                            {
+                                new Certificate()
+                                {
+                                    FileName = model.FileName
+                                }
+                            }
+
+                    };
+
+
+                    data.PartnerIdentityProviderConfigurations.Add(partnerIdentityProvider);
+                    _context.SaveChanges();
+                    //return RedirectToAction("/Index");
+
+                }
+            }
+            catch (NullReferenceException ex)
+            {
+                return new EmptyResult();
+            }
+            
+
+            return View(model);
+        }
+ 
+     
+            //samlConfiguration.LocalServiceProviderConfiguration = new LocalServiceProviderConfiguration()
+            //{
+            //    Name = "https://EMSuite",
+            //    Description = "EMSuite",
+            //    AssertionConsumerServiceUrl = "https://localhost:44306/SAML/AssertionConsumerService",
+            //    SingleLogoutServiceUrl = "https://localhost:44306/SAML/SingleLogoutService",
+            //    LocalCertificates = new List<Certificate>()
+            //        {
+            //            new Certificate()
+            //            {
+            //                FileName = "certificates/sp.pfx",
+            //                Password = "password"
+            //            }
+            //        }
+            //};
+
+            //samlConfiguration.PartnerIdentityProviderConfigurations = new List<PartnerIdentityProviderConfiguration>()
+            //{
+            //        new PartnerIdentityProviderConfiguration()
+            //        {
+            //            Name = "http://www.okta.com/exk3n5fc0drZ7m57X697",
+            //            Description = "Okta",
+            //            SignAuthnRequest = true,
+            //            SignLogoutRequest = true,
+            //            SignLogoutResponse = true,
+            //            SingleSignOnServiceUrl = "https://trial-4007665.okta.com/app/trial-4007665_emsuite_1/exk3n5fc0drZ7m57X697/sso/saml",
+            //            SingleLogoutServiceUrl = "https://trial-4007665.okta.com/app/trial-4007665_emsuite_1/exk3n5fc0drZ7m57X697/slo/saml",
+            //            PartnerCertificates = new List<Certificate>()
+            //            {
+            //                new Certificate()
+            //                {
+            //                    FileName = "certificates/okta.cer"
+            //                }
+            //            }
+            //        },
+            //        new PartnerIdentityProviderConfiguration()
+            //        {
+            //            Name = "https://app.onelogin.com/saml/metadata/a79374a7-67e0-4a7a-9abc-3ce9a38b4ab1",
+            //            Description = "OneLogin",
+            //            SignAuthnRequest = true,
+            //            SignLogoutRequest = true,
+            //            SignLogoutResponse = true,
+            //            SingleSignOnServiceUrl = "https://ellab.onelogin.com/trust/saml11/http-post/sso/a79374a7-67e0-4a7a-9abc-3ce9a38b4ab1",
+            //            SingleLogoutServiceUrl = "https://ellab.onelogin.com/trust/saml2/http-redirect/slo/1938103",
+            //            PartnerCertificates = new List<Certificate>()
+            //            {
+            //                new Certificate()
+            //                {
+            //                    FileName = "certificates/onelogin.cer"
+            //                }
+            //            }
+            //        }
+            // };
+
+            // _context.SamlConfigurations.Add(samlConfiguration);
+            //_context.SaveChanges();
+
+          
+        
+
+        // GET: SamlConfigurations/Edit/5
+        //[HttpGet]
+        //public async Task<IActionResult> Edit(int? id, SamlConfigurationViewModel model)
+        //{
+        //    if (id == null || _context.SamlConfigurations == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var samlConfiguration = _context.SamlConfigurations.FindAsync(id);
+        //    if (samlConfiguration == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    //ViewData["PartnerIdentityProviderConfigurationId"] = new SelectList(_context.PartnerIdentityProviderConfigurations, "Id", "Id", model.Id);
+        //    //ViewData["LocalServiceProviderConfigurationId"] = new SelectList(_context.LocalServiceProviderConfigurations, "Id", "Id", samlConfiguration.LocalServiceProviderConfigurationId);
+        //    return View(model);
+        //}
+
+        // POST: SamlConfigurations/Edit/5
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, SamlConfigurationViewModel model)
+        {
+            if (id != model.Id)
+            {
+                return NotFound();
+            }
+
+            //if (ModelState.IsValid)
+            //{
+            try
+            {
+                var samlConfiguration = new SamlConfiguration()
+                {
+                    //LocalServiceProviderConfiguration = new LocalServiceProviderConfiguration()
+                    //{
+                    //    Name = "https://EMSuite",
+                    //    Description = "",
+                    //    AssertionConsumerServiceUrl = "https://localhost:44306/SAML/AssertionConsumerService",
+                    //    SingleLogoutServiceUrl = "https://localhost:44306/SAML/SingleLogoutService",
+                    //    ArtifactResolutionServiceUrl = "https://localhost:44306/SAML/ArtifactResolutionService",
+                    //    LocalCertificates = new List<Certificate>()
+                    //    {
+                    //        new Certificate()
+                    //        {
+                    //            FileName = "certificates/sp.pfx",
+                    //            Password = "password"
+                    //        }
+                    //    }
+                    //},
+                    PartnerIdentityProviderConfigurations = new List<PartnerIdentityProviderConfiguration>()
+                        {
+                            new PartnerIdentityProviderConfiguration()
+                            {
+                                Id = model.Id,
+                                Name = model.Name,
+                                Description = model.Description,
+                                SignAuthnRequest = true,
+                                SignLogoutRequest = true,
+                                SignLogoutResponse = true,
+                                SingleSignOnServiceUrl = model.SingleSignOnServiceUrl,
+                                SingleLogoutServiceUrl = model.SingleLogoutServiceUrl,
+                                PartnerCertificates = new List<Certificate>()
+                                {
+                                    new Certificate()
+                                    {
+
+                                        FileName = model.FileName
+                                    }
+                                }
+                            }
+                        }
+                };
+
+                _context.SamlConfigurations.Update(samlConfiguration);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "SamlConfiguration");
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SamlConfigurationExists(model.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    ViewData["Title"] = "error";
+                }
+            }
+
+            return View(model);
+        }
+
+        // GET: SamlConfigurations/Delete/5
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id, SamlConfigurationViewModel model)
+        {
+            if (id == null || _context.SamlConfigurations == null)
+            {
+                return NotFound();
+            }
+
+
+            await _context.SamlConfigurations
+            .Include(p => p.PartnerIdentityProviderConfigurations)
+            //.Include(p => p.PartnerIdentityProviderConfigurations)
+            .FirstOrDefaultAsync(p => p.Id == id);
+
+
+            return View(model);
+        }
+
+        //POST: SamlConfigurations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id, SamlConfigurationViewModel model, SamlConfiguration samlConfiguration)
+        {
+
+            var idp = _context.SamlConfigurations.Find(2027);
+
+            _context.SamlConfigurations.Remove(idp);
+            _context.SaveChanges();
+
+            return View(model);
+        }
+
+
+
+
+        private bool SamlConfigurationExists(int id)
+        {
+            return _context.SamlConfigurations.Any(e => e.Id == id);
+        }
+
+
+    }
+    }
+
---
+++ SamlController.cs
@@ -0,0 +1,266 @@
+﻿using ComponentSpace.Saml2;
+using ComponentSpace.Saml2.Metadata.Export;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Text;
+using System.Xml;
+using EMSuite.ViewModels;
+using EMSuite.Data;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Newtonsoft.Json.Linq;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EMSuite.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class SamlController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IUserStore<ApplicationUser> _userStore;
+        private readonly ISamlServiceProvider _samlServiceProvider;
+        private readonly IConfigurationToMetadata _configurationToMetadata;
+        private readonly IConfiguration _configuration;
+        private readonly ApplicationDbContext _context;
+
+        public SamlController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            IUserStore<ApplicationUser> userStore,
+            ISamlServiceProvider samlServiceProvider,
+            IConfigurationToMetadata configurationToMetadata,
+            IConfiguration configuration,
+            ApplicationDbContext context)
+
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _userStore = userStore;
+            _samlServiceProvider = samlServiceProvider;
+            _configurationToMetadata = configurationToMetadata;
+            _configuration = configuration;
+            _context = context;
+
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        
+        public async Task<IActionResult> InitiateSingleSignOn(ApplicationUser model, string? returnUrl = null)
+        {
+            //var partnerAdfs = _configuration["Adfs"];
+            //var partnerAzure = _configuration["Azure"];
+            //var partnerCentrify = _configuration["Centrify"];
+            //var partnerGoogle = _configuration["Google"];
+            var partnerOkta = _configuration["PartnerOkta"];
+            var partnerOneLogin = _configuration["PartnerOneLogin"];
+            // var partnerPingOne = _configuration["PingOne"];
+            //var partnerSalesforce = _configuration["PartnerSalesforce"];
+            // var partnerShibboleth = _configuration["Shibboleth"];
+
+
+
+            // Retrieve the value from the input field
+            string inputValue = model.Company;
+            var user = _userManager.Users.FirstOrDefault(u => u.Company == inputValue);
+            //var user = _context.Users.FirstOrDefault(u => u.Email == inputValue);
+
+            if (user != null)
+            {
+                // The value exists in the custom column
+                switch (user.IdentityProvider)
+                {
+
+                    //case "Adfs":
+                    //    await _samlServiceProvider.InitiateSsoAsync(partnerAdfs, returnUrl);
+                    //    break;
+                    //case "Azure":
+                    //    await _samlServiceProvider.InitiateSsoAsync(partnerAzure, returnUrl);
+                    //    break;
+                    //case "Centrify":
+                    //    await _samlServiceProvider.InitiateSsoAsync(partnerCentrify, returnUrl);
+                    //    break;
+                    //case "Google":
+                    //    await _samlServiceProvider.InitiateSsoAsync(partnerGoogle, returnUrl);
+                    //    break;
+                    case "Okta":
+                        await _samlServiceProvider.InitiateSsoAsync(partnerOkta, returnUrl);
+                        break;
+                    case "OneLogin":
+                        await _samlServiceProvider.InitiateSsoAsync(partnerOneLogin, returnUrl);
+                        break;
+                    //case "PingOne":
+                    //    await _samlServiceProvider.InitiateSsoAsync(partnerPingOne, returnUrl);
+                    //    break;
+                    //case "Salesforce":
+                    //await _samlServiceProvider.InitiateSsoAsync(partnerSalesforce, returnUrl);
+                    //break;
+                    //case "Shibboleth":
+                    //    await _samlServiceProvider.InitiateSsoAsync(partnerShibboleth, returnUrl);
+                    //    break;
+                    default:
+                        ViewData["Title"] = "Company Domain does not exist";
+                        break;
+
+                }
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "The company name or the username does not exist.");
+                return new EmptyResult();
+            }
+
+            return new EmptyResult();
+        }
+
+        public async Task<IActionResult> InitiateSingleLogout(string? returnUrl = null)
+        {
+            //Request logout at the identity provider.
+            await _samlServiceProvider.InitiateSloAsync(relayState: returnUrl);
+
+            return new EmptyResult();
+        }
+
+        public async Task<IActionResult> AssertionConsumerService(ApplicationUser model)
+        {
+
+            //Receive and process the SAML assertion contained in the SAML response.
+            //The SAML response is received either as part of IdP-initiated or SP-initiated SSO
+            var ssoResult = await _samlServiceProvider.ReceiveSsoAsync();
+
+
+          
+            
+        
+            //Automatically provision the user.
+            //If the user doesn't exist locally then create the user.
+            //Automatic provisioning is an optional step.
+
+            //var user1 = await _userManager.GetUserName(ssoResult.UserID)
+
+           var user = await _userManager.FindByNameAsync(ssoResult.UserID);
+            //var user = await _userManager.FindByNameAsync(ssoResult.UserID);
+
+           
+            if (user == null)
+            {
+                //user = { model.Company, model.UserName};
+                user = new ApplicationUser { UserName = ssoResult.UserID, Email = ssoResult.UserID };
+
+                var result = await _userManager.CreateAsync(user);
+                //user.Company = model.Company;
+
+                if (!result.Succeeded)
+                {
+                    throw new Exception($"The user {ssoResult.UserID} couldn't be created - {result}");
+                }
+
+                //For demonstration purposes, create some additional claims.
+                if (ssoResult.Attributes != null)
+                {
+                    var samlAttribute = ssoResult.Attributes.SingleOrDefault(a => a.Name == ClaimTypes.Email);
+
+                    if (samlAttribute != null)
+                    {
+                        await _userManager.AddClaimAsync(user, new Claim(ClaimTypes.Email, samlAttribute.ToString()));
+                    }
+
+                    samlAttribute = ssoResult.Attributes.SingleOrDefault(a => a.Name == ClaimTypes.GivenName);
+
+                    if (samlAttribute != null)
+                    {
+                        await _userManager.AddClaimAsync(user, new Claim(ClaimTypes.GivenName, samlAttribute.ToString()));
+                    }
+
+                    samlAttribute = ssoResult.Attributes.SingleOrDefault(a => a.Name == ClaimTypes.Surname);
+
+                    if (samlAttribute != null)
+                    {
+                        await _userManager.AddClaimAsync(user, new Claim(ClaimTypes.Surname, samlAttribute.ToString()));
+                    }
+                }
+            }
+
+            //Automatically login using the asserted identity.
+            await _signInManager.SignInAsync(user, isPersistent: false);
+
+            // Redirect to the target URL if specified.
+            if (!string.IsNullOrEmpty(ssoResult.RelayState))
+            {
+                return LocalRedirect(ssoResult.RelayState);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public async Task<IActionResult> SingleLogoutService()
+        {
+            //Receive the single logout request or response.
+            //If a request is received then single logout is being initiated by the identity provider.
+            //If a response is received then this is in response to single logout having been initiated by the service provider.
+            var sloResult = await _samlServiceProvider.ReceiveSloAsync();
+
+            if (sloResult.IsResponse)
+            {
+                //SP - initiated SLO has completed.
+                if (!string.IsNullOrEmpty(sloResult.RelayState))
+                {
+                    return LocalRedirect(sloResult.RelayState);
+                }
+
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                //Logout locally.
+                await _signInManager.SignOutAsync();
+
+                //Respond to the IdP-initiated SLO request indicating successful logout.
+                await _samlServiceProvider.SendSloAsync();
+            }
+
+            return new EmptyResult();
+        }
+
+        public async Task<IActionResult> ArtifactResolutionService()
+        {
+            //Resolve the HTTP artifact.
+            //This is only required if supporting the HTTP-Artifact binding.
+            await _samlServiceProvider.ResolveArtifactAsync();
+
+            return new EmptyResult();
+        }
+
+        public async Task<IActionResult> ExportMetadata()
+        {
+            var entityDescriptor = await _configurationToMetadata.ExportAsync();
+            var xmlElement = entityDescriptor.ToXml();
+
+            Response.ContentType = "text/xml";
+            Response.Headers.Add("Content-Disposition", "attachment; filename=\"metadata.xml\"");
+
+            var xmlWriterSettings = new XmlWriterSettings()
+            {
+                Async = true,
+                Encoding = Encoding.UTF8,
+                Indent = true,
+                OmitXmlDeclaration = true
+            };
+
+            using (var xmlWriter = XmlWriter.Create(Response.Body, xmlWriterSettings))
+            {
+                xmlElement.WriteTo(xmlWriter);
+                await xmlWriter.FlushAsync();
+            }
+
+            return new EmptyResult();
+        }
+    }
+}
